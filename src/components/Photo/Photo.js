03c1ff4d35605c593299240d0798c7a904238b93
jonyrahman626 <jonyrahman15@gmail.com>
2021-05-05 20:24:20 +0600

    single photo

--- src/components/Photo/Photo.js
+++ src/components/Photo/Photo.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Photo extends Component {
+
+     state = {
+         photo: [],
+         loading: true,
+     }
 
     componentDidMount() {
         let search = window.location.search;
         let params = new URLSearchParams(search);
-        let foo = params.get('id');
-        console.log(foo);
+        let photo_id = params.get('id');
+
+        axios.get('https://api.unsplash.com/photos/'+ photo_id +'/?client_id=30lbu7849dnGgOlcluC9ufK-zLWHxdSaP2GtOpJD2IE').then(
+            res => this.setState({
+                photo: res.data,
+                loading: false,
+            })
+        )
     };
     
     render() {
+
+        console.log(this.state.photo);
         return (
             <div>
                 <h1>Single Photo Is Here</h1>
