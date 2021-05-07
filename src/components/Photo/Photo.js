1713da6770185aedd6a0395f713bcf1a6b543505
jonyrahman626 <jonyrahman15@gmail.com>
2021-05-07 20:08:32 +0600

    single photo

--- src/components/Photo/Photo.js
+++ src/components/Photo/Photo.js
@@ -30,15 +30,15 @@ class Photo extends Component {
                 <section>
                     <div className="container-fluid">
                         <div className="single_photo">
-                            <h3>{photo.title}</h3>
-                            <p>{photo.alt_description}</p>
+                            {photo.title ? <h3>{photo.title}</h3> : ''}
+                            {photo.alt_description ? <p>{photo.alt_description}</p> : ''}
                             <ul>
-                                <li><label htmlFor="uploadBy">Uploaded By</label> {photo.user.first_name} {photo.user.last_name}</li>
+                                <li><label htmlFor="uploadBy">Uploaded By</label> {photo.first_name ? photo.first_name : ''} {photo.last_name ? photo.last_name :  ''}</li>
                                 <li><label htmlFor="upDate">Upload Date</label>{photo.updated_at}</li>
-                                <li><label htmlFor="cmModel">Camera Model</label>{photo.exif.model}</li>
+                                <li><label htmlFor="cmModel">Camera Model</label>{photo.exif && photo.exif.model}</li>
                             </ul>
                         </div>
-                        <img src={photo.urls.full} alt="" />
+                        <img className="w-100" src={photo.urls && photo.urls.full} alt="" />
                     </div>
                 </section>
             </div>
